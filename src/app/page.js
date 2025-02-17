f6139949c9dab99727f3c6dcbff0538328d7f245
24LU4966 24LU4966 <bsskaa.kai123@gmail.com>
2025-02-17 12:05:58 +0800

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,105 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import { useState } from "react"
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+const StepOne = ({props}) => {
+  return <div className="h-screen bg-gray-100 flex justify-center items-center">
+    <div className="w-[480px] h-[655] bg-white rounded-[10px]">
+      <div className="w-[480px] h-[655] py-4 px-8">
+
+        <div>
+          <img className="w-[60px] h-[60px] bold" src="logo.png"></img>
+          <p className="text-[26px] font-bold">Join us! 😎</p>
+          <p className="text-gray-400 text-[18px]">Please provide all current information accurately.</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="pt-10">
+          <div className="w-[416px] h-[68px]">
+            <p className="text-[14px] font-bold">First name</p>
+            <input className="h-[44px] w-[416px] border-[2px] border-solid rounded-[5px] placeholder:pl-3" type="text" placeholder="Your First Name"></input>
+          </div>
+
+          <div className="mt-2">
+            <p className="text-[14px] font-bold">Last name</p>
+            <input className="h-[44px] w-[416px] border-[2px] border-solid rounded-[5px] placeholder:pl-3" type="text" placeholder="Your Last Name"></input>
+          </div>
+
+          <div className="mt-2">
+            <p className="text-[14px] font-bold">Username</p>
+            <input className="h-[44px] w-[416px] border-[2px] border-solid rounded-[5px] placeholder:pl-3" type="text" placeholder="Your Username"></input>
+          </div>
+        </div>
+
+        <div className="flex justify-center mt-40 mb-5">
+          <button className="bg-black text-white h-[44px] w-[416px] rounded-[8px] font-bold" onClick={props} type="button"> Continue</button>
+        </div>
+      </div>
     </div>
-  );
+  </div>
+}
+
+const StepTwo = ({props1}) => {
+  return <div className="h-screen bg-gray-100 flex justify-center items-center">
+    <div className="w-[480px] h-[655] bg-white rounded-[10px]">
+      <div className="w-[480px] h-[655] py-4 px-8">
+
+        <div>
+          <img className="w-[60px] h-[60px] bold" src="logo.png"></img>
+          <p className="text-[26px] font-bold">Join us! 😎</p>
+          <p className="text-gray-400 text-[18px]">Please provide all current information accurately.</p>
+        </div>
+
+        <div className="pt-10">
+          <div className="w-[416px] h-[68px]">
+            <p className="text-[14px] font-bold">Email</p>
+            <input className="h-[44px] w-[416px] border-[2px] border-solid rounded-[5px] placeholder:pl-3" type="text" placeholder="Your email"></input>
+          </div>
+
+          <div className="mt-2">
+            <p className="text-[14px] font-bold">Phone number</p>
+            <input className="h-[44px] w-[416px] border-[2px] border-solid rounded-[5px] placeholder:pl-3" type="text" placeholder="Your phone number here"></input>
+          </div>
+
+          <div className="mt-2">
+            <p className="text-[14px] font-bold">Password</p>
+            <input className="h-[44px] w-[416px] border-[2px] border-solid rounded-[5px] placeholder:pl-3" type="password" placeholder="Your password"></input>
+          </div>
+
+          <div className="mt-2">
+            <p className="text-[14px] font-bold">Confirm password</p>
+            <input className="h-[44px] w-[416px] border-[2px] border-solid rounded-[5px] placeholder:pl-3" type="password" placeholder="Confirm your password"></input>
+          </div>
+        </div>
+
+        <div className="flex justify-between mt-40 mb-5 ">
+          <button className="bg-white text-black h-[44px] w-[128px] rounded-[8px] font-bold border-[2px]" onClick={props1} type="button"> Back</button>
+          <button className="bg-black text-white h-[44px] w-[280px] rounded-[8px] font-bold" type="button"> Continue</button>
+        </div>
+      </div>
+    </div>
+  </div>
+}
+export default function Home() {
+
+  const [currentStep, setCurrentStep] = useState(0);
+
+  const Component = [StepOne, StepTwo][currentStep]
+
+  const nextStep = () => {
+    if (1 > currentStep) {
+      setCurrentStep(currentStep + 1);
+    }
+  }
+
+  const previousStep = () => {
+    setCurrentStep(currentStep - 1);
+  }
+
+  return (
+    <div>
+      <form>
+        <Component props = {nextStep} props1 = {previousStep} ></Component>
+      </form>
+    </div>
+  )
 }
